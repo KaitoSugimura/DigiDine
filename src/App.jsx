c49b268ea17e980d723ba9a3f7631c8a89830235
Kaito Sugimura <sugimurakaito@gmail.com>
2024-10-28 12:34:48 -0600

    Add scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -16,20 +16,95 @@ function App() {
     {}
   );
 
-  const [currentCategory, setCurrentCategory] = useState("Featured");
+  const [currentCategory, _setCurrentCategory] = useState("Featured");
+
+  const [orderList, setOrderList] = useState([]);
+
+  const [pageNo, setPageNo] = useState(0);
+
+  const setCurrentCategory = (category) =>{
+
+    _setCurrentCategory(category);
+    setPageNo(0);
+  }
+
+  const nextPage = ()=>{
+    setPageNo((prev)=>{
+      return prev + 1;    
+    })
+  }
+
+  const backPage = ()=>{
+    setPageNo((prev)=>{
+      return prev - 1;    
+    })
+  }
+
+  const addOrder = (name)=>{
+    setOrderList((prev)=>{
+      let done = false;
+      prev.forEach((item, i)=>{
+        if(item[0] == name){
+          item[1]++;
+          done = true;
+        }
+      });
+      if(!done){
+        prev.push([name, 1]);
+      }
+      return [...prev];
+    })
+  }
+
+  
 
   return (
-    <CategoryContext.Provider value={{ currentCategory, setCurrentCategory }}>
+    <CategoryContext.Provider value={{ currentCategory, setCurrentCategory, orderList, addOrder }}>
+      {/* Delte later */}
+      <div style={{
+              position: "fixed",
+              top: 0,
+              right: 0,
+              backgroundColor: "white",
+              zIndex: 10000,
+              padding: 10
+            }}>
+              {orderList.map((item, i) => (
+                <p key={i}>
+                  {item[0]} x{item[1]}
+                </p>
+              ))}
+            </div>
       <div className={styles.root}>
         <div className={styles.app}>
-          <div>
+          <div className={styles.rootGrid}>
             <div>
               <h1>{currentCategory}</h1>
             </div>
+            <div className={styles.mainGrid}>
+              <button 
+              disabled={pageNo==0}
+              onClick={()=>{
+                backPage();
+              }}>
+                {"<"}
+              </button>
             <div className={styles.cardGrid}>
-              {foodItems[currentCategory].map((item, i) => (
-                <FoodCard key={currentCategory + i} {...item} />
-              ))}
+              {foodItems[currentCategory].map((item, i) => {
+                if(i >= pageNo*8 && i < (pageNo*8 + 8)){
+                  return (
+                    <FoodCard key={currentCategory + i} {...item} />
+                  )
+                }
+              })}
+            </div>
+            <button 
+            disabled={foodItems[currentCategory].length <= (pageNo*8 + 8)}
+            onClick={()=>{
+                nextPage();
+              }}>
+                {">"}
+              </button>
             </div>
             <div className={styles.navBar}>
               <MainNavbar />
--- src/Main/FoodCard.jsx
+++ src/Main/FoodCard.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import styles from "./FoodCard.module.css";
+import { useCategoryContext } from "../App";
 
 export default function FoodCard({ image, title, price }) {
-  console.log(image);
+
+  const { addOrder } = useCategoryContext();
+
   return (
     <div className={styles.cardRoot}>
       <div className={styles.imageParent}>
@@ -11,7 +14,9 @@ export default function FoodCard({ image, title, price }) {
       <div className={styles.content}>
         <h3>{title}</h3>
         <p>${price}</p>
-        <button className={styles.addButton}>+</button>
+        <button className={styles.addButton} onClick={()=>{
+          addOrder(title);
+        }}>+</button>
       </div>
     </div>
   );
