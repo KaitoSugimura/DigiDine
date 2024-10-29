1efd6af60d0c96ed3c6d36aa3198037fde715da3
Aoi Ueki <129098557+Aoi-U@users.noreply.github.com>
2024-10-29 16:49:59 -0600

    added detail category in FoodData.js

--- src/FoodData.js
+++ src/FoodData.js
@@ -6,6 +6,7 @@ export default [
         title: "Raw Chicken",
         price: 5.99,
         image: "Chicken/raw-chicken.png",
+        detail: "Simply just raw chicken. No seasoning, no cooking, just raw chicken.",
       },
       {
         title: "Pasta",
@@ -27,54 +28,63 @@ export default [
         price: 5.99,
         calories: 1030,
         image: "Burgers/big-arch.jfif",
+        detail: "Two quarter pound* 100% Canadian beef patties, layered with three slices of white processed cheese, and topped with crispy onions, slivered onions, pickles, lettuce and delicious Big Arch sauce, served on a toasted sesame and poppy seed bun.",
       },
       {
         title: "Cheeseburger Deluxe",
         price: 5.99,
         calories: 340,
         image: "Burgers/cheeseburger-deluxe.jfif",
+        detail: "Your favourite cheeseburger is now made deluxe! A slice of processed cheddar cheese on a juicy, 100% Canadian beef patty with pickles and onions, ketchup, mustard, mayo style sauce, tomato, and shredded lettuce on a toasted bun."
       },
       {
         title: "Quarter Pounder with Cheese",
         price: 5.99,
         calories: 520,
         image: "Burgers/quarter-pounder-with-cheese.jfif",
+        detail: "A quarter pound* of 100% Canadian beef and two slices of melty processed cheese on a toasted sesame seed bun."
       },
       {
         title: "Double Quarter Pounder With Cheese",
         price: 7.99,
         calories: 740,
         image: "Burgers/double-quarter-pounder-with-cheese.jfif",
+        detail: "Two quarter pound* 100% Canadian beef patties. Layered with two slices of melty cheese, slivered onions and pickles on sesame seed bun."
       },
       {
         title: "WcDouble",
         price: 2.19,
         calories: 390,
         image: "Burgers/mcdouble.jfif",
+        detail: "A slice of melted processed cheese between two 100% Canadian beef patties, topped with pickles, onions, ketchup and mustard on a toasted bun."
       },
       {
         title: "Cheeseburger",
         price: 1.39,
         calories: 300,
         image: "Burgers/cheeseburger.jfif",
+        detail: "A slice of melted processed cheese on a juicy, 100% Canadian beef patty with tangy pickles and onions, ketchup and mustard on a freshly-toasted bun."
       },
       {
         title: "Hamburger",
         price: 1.19,
         calories: 250,
         image: "Burgers/hamburger.jfif",
+        detail: "The comforting taste of the juicy and delicious 100% Canadian beef burger, topped with tangy pickles, ketchup, mustard and the sweet bite of onion, all on a freshly toasted bun. Just like you remember."
       },
       {
         title: "Double Cheeseburger",
         price: 2.19,
         calories: 440,
         image: "Burgers/double-cheeseburger.jfif",
+        detail: "We make it like we always have: two slices of processed cheese with two 100% Canadian beef patties, pickles, onions, ketchup and mustard on a freshly toasted bun."
       },
       {
         title: "Big Wac",
         price: 5.99,
         calories: 540,
         image: "Burgers/big-mac.jfif",
+        detail: "Nothing compares to two 100% Canadian beef patties, special sauce, crisp lettuce, processed cheese, pickles and onions on a toasted sesame seed bun."
       },
     ],
   ],
@@ -85,85 +95,99 @@ export default [
         title: "Raw Chicken",
         price: 50.00,
         calories: 200,
-        image: "Chicken/raw-chicken.png"
+        image: "Chicken/raw-chicken.png",
+        detail: "Just raw chicken. No seasoning, no cooking, just raw chicken.",
       },
       {
         title: "Firecracker Bacon WcCrispy",
         price: 6.99,
         calories: 200,
-        image: "Chicken/Firecracker-McCrispy-With-Bacon.jpg"
+        image: "Chicken/Firecracker-McCrispy-With-Bacon.jpg",
+        detail: "Crispy chicken sandwich topped with a creamy sweet and spicy sauce, hickory-smoked bacon and crispy jalapenos. Made with tender and juicy 100% Canadian-raised seasoned chicken and served on a soft potato bun, it has a taste that’s unmistakably McDonald’s."
       },
       {
         title: "Firecracker WcCrispy",
         price: 5.99,
         calories: 200,
-        image: "Chicken/Firecracker-McCrispy.jpg"
+        image: "Chicken/Firecracker-McCrispy.jpg",
+        detail: "Crispy chicken sandwich topped with a creamy sweet and spicy sauce and crispy jalapenos. Made with tender and juicy 100% Canadian-raised seasoned chicken and served on a soft potato bun, it has a taste that’s unmistakably WcDonald’s"
       },
       {
         title: "Grilled Chicken",
         price: 3.99,
         calories: 200,
-        image: "Chicken/Grilled-Chicken-Sandwich.jpg"
+        image: "Chicken/Grilled-Chicken-Sandwich.jpg",
+        detail: "Made with tender and juicy 100% Canadian-raised grilled seasoned chicken. Topped with shredded lettuce, creamy mayo-style sauce and served on a soft potato bun for that WcD’s flavour you crave."
       },
       {
         title: "Junior Chicken",
         price: 2.99,
         calories: 200,
-        image: "Chicken/Junior-Chicken.jpg"
+        image: "Chicken/Junior-Chicken.jpg",
+        detail: "You don’t need to compromise with the great taste of a Junior Chicken sandwich, made with chicken raised on Canadian farms, at a price you can always feel good about."
       },
       {
         title: "WcChicken",
         price: 2.99,
         calories: 200,
-        image: "Chicken/McChicken.jpg"
+        image: "Chicken/McChicken.jpg",
+        detail: "Breaded seasoned chicken and crisp lettuce, topped with our Mayo-Style Sauce. Some ingredients are just meant to be together."
       },
       {
         title: "WcCrispy",
         price: 2.99,
         calories: 200,
-        image: "Chicken/McCrispy.jpg"
+        image: "Chicken/McCrispy.jpg",
+        detail: "The WcCrispy is a crispy chicken sandwich made with tender and juicy 100% Canadian-raised seasoned chicken. It’s topped with shredded lettuce and creamy mayo-style sauce and served on a soft potato bun for a taste that’s so WcDonald’s."
       },
       {
         title: "WcCrispy Bacon Deluxe",
         price: 3.99,
         calories: 200,
-        image: "Chicken/McCrispy-Bacon-Deluxe.jpg"
+        image: "Chicken/McCrispy-Bacon-Deluxe.jpg",
+        detail: "Bacon and a slice of fresh tomato take the WcCrispy to the next level. Made with crispy, tender and juicy 100% Canadian-raised seasoned chicken, then topped with shredded lettuce, creamy mayo-style sauce and served on a soft potato bun for a taste that’s so WcDonald’s."
       },
       {
         title: "Sweet Chili Junior Chicken",
         price: 3.99,
         calories: 200,
-        image: "Chicken/Sweet-Chili-Junior-Chicken.jpg"
+        image: "Chicken/Sweet-Chili-Junior-Chicken.jpg",
+        detail: "Your tastebuds will be singing a whole new tune with a remixed Sweet Chili Junior Chicken made with Canadian-raised crispy seasoned chicken, topped with crunchy shredded lettuce, tangy sweet chili sauce and mayo-style sauce, all on a toasty bun."
       },
       {
         title: "WcCrispy 🌶️",
         price: 2.99,
         calories: 200,
-        image: "Chicken/Spicy-McCrispy.jpg"
+        image: "Chicken/Spicy-McCrispy.jpg",
+        detail: "The Spicy WcCrispy is a crispy chicken sandwich topped with our signature habanero sauce. Made with tender and juicy 100% Canadian-raised seasoned chicken and served on a soft potato bun, it has a taste that’s unmistakably WcDonald’s."
       },
       {
         title: "Bacon Grilled Chicken 🌶️",
         price: 4.99,
         calories: 200,
-        image: "Chicken/Spicy-Bacon-Deluxe-Grilled-Chicken-Sandwich.jpg"
+        image: "Chicken/Spicy-Bacon-Deluxe-Grilled-Chicken-Sandwich.jpg",
+        detail: "The Spicy Bacon Deluxe Grilled Chicken Sandwich is big on everything, including heat. Made with 100% Canadian-raised grilled seasoned chicken on a soft potato bun, topped with shredded lettuce, hickory-smoked bacon, tomato and our spicy Habanero sauce, this sandwich kicks juicy and tender up to the next level."
       },
       {
         title: "Grilled Chicken 🌶️",
         price: 3.99,
         calories: 200,
-        image: "Chicken/Spicy-Grilled-Chicken-Sandwich.jpg"
+        image: "Chicken/Spicy-Grilled-Chicken-Sandwich.jpg",
+        detail: "Made with tender and juicy 100% Canadian-raised grilled seasoned chicken, and topped with shredded lettuce and our signature Habanero sauce, it’s a spicy-lovers dream served on a soft potato bun. "
       },
       {
         title: "Habanero WcChicken 🌶️",
         price: 3.99,
         calories: 200,
-        image: "Chicken/Spicy-Habanero-McChicken.jpg"
+        image: "Chicken/Spicy-Habanero-McChicken.jpg",
+        detail: "Our classic WcChicken packed with a punch! This spicy Habanero WcChicken is topped with our creamy Habanero sauce. Perfectly breaded seasoned chicken, crisp shredded lettuce, all on a freshly toasted sesame bun. Some ingredients are just meant to be together."
       },
       {
         title: "WcCrispy Bacon Deluxe 🌶️", 
         price: 5.99,
         calories: 200,
-        image: "Chicken/Spicy-McCrispy-Bacon-Delux.jpg"
+        image: "Chicken/Spicy-McCrispy-Bacon-Delux.jpg",
+        detail: "The Spicy Bacon Deluxe WcCrispy is big on everything, including heat. With our southern-style fried chicken patty on a soft potato bun, topped with shredded lettuce, hickory-smoked bacon, tomato and our spicy Habanero sauce, this sandwich kicks crispy, juicy and tender up to the next level."
       },
     ],
   ],
@@ -174,11 +198,13 @@ export default [
         title: "West Side Wario's Carbonara",
         price: 9.99,
         image: "Pasta/Marios-pasta.jpg",
+        detail: "Spaghetti noodles in a creamy Alfredo sauce with bacon, roasted garlic, Italian seasoning and Parmesan cheese."
       },
       {
         title: "Classic Jorimee Pasta",
         price: 7.99,
         image: "Pasta/jolibee-pasta.jpg",
+        detail: "Spaghetti noodles in a sweet Filipino style spaghetti sauce with ground beef, hot dog slices, and shredded cheese."
       },
     ],
   ],
@@ -189,26 +215,31 @@ export default [
         title: "CFK Macaroni Salad",
         price: 3.99,
         image: "Salads/kfc-salad.png",
+        detail: "Macaroni noodles in a creamy dressing with shredded carrots, celery, and onions."
       },
       {
         title: "WcDonald Classic Salad",
         price: 6.99,
         image: "Salads/Mcdonald-salad.png",
+        detail: "A blend of premium lettuces, grape tomatoes, shaved carrots, real cheddar cheese, and slices of crispy chicken strips on top."
       },
       {
         title: "West Side Wario's Salad",
         price: 5.99,
         image: "Salads/Marios-salad.jpg",
+        detail: "A house style salad with mixed greens, grape tomatoes, cucumbers, and onions."
       },
       {
         title: "Bendy's Caesar Salad",
         price: 6.99,
         image: "Salads/wendys-caesar.png",
+        detail: "A classic caesar salad with romaine lettuce and big crispy croutons."
       },
       {
         title: "Bendy's Tacos Caesar Salad",
         price: 7.99,
         image: "Salads/wendys-taco-salad.png",
+        detail: "Our classic Bendy's Caesar Salad with a mix of taco seasoned beef, shredded cheddar cheese, and tortilla strips."
       }
     ],
   ],
@@ -219,21 +250,25 @@ export default [
         title: "WcDonald's Fries",
         price: 1.99,
         image: "Sides/wcdonald-fries.jpg",
+        detail: "Our World Famous Fries are crispy and golden on the outside and fluffy on the inside."
       },
       {
         title: "W&A Onion Rings",
         price: 2.99,
         image: "Sides/onion-rings.png",
+        detail: "Thick-cut, battered and breaded onion rings."
       },
       {
         title: "Big Caesers Rational Bread",
         price: 2.99,
         image: "Sides/crazy-bread.jpg",
+        detail: "Freshly baked breadsticks topped with garlic butter and Parmesan cheese."
       },
       {
         title: "Bendy's Poutine",
         price: 5.99,
         image: "Sides/wendys-poutine.png",
+        detail: "Our classic poutine with cheese curds and gravy."
       }
     ],
   ],
@@ -244,16 +279,19 @@ export default [
         title: "WcDonald's Vanilla Ice Cream",
         price: 1.99,
         image: "Dessert/mcdonalds-vanilla-icecream.jpg",
+        detail: "Our creamy vanilla soft serve in a crispy cone."
       },
       {
         title: "Fairy King Oreo Blizzard",
         price: 3.99,
         image: "Dessert/OreoBlizzard.jpg",
+        detail: "Our classic Blizzard made with Oreo cookie pieces and vanilla soft serve."
       },
       {
         title: "Pie",
         price: 5.99,
         image: "NoImage.png",
+        detail: "A classic apple pie."
       },
     ],
   ],
@@ -264,66 +302,79 @@ export default [
         title: "Worlds Most Expensive Water",
         price: 100000,
         image: "Drink/expensive-water.png",
+        detail: "The most expensive water in the world. It's just water.",
       },
       {
         title: "Regular Water",
         price: 0,
         image: "Drink/water.jpg",
+        detail: "Just regular water.",
       },
       {
         title: "Oi Ocha",
         price: 2.99,
         image: "Drink/oiocha.png",
+        detail: "Japanese green tea.",
       },
       {
         title: "Kirin Milk Tea",
         price: 2.99,
         image: "Drink/kirinmilktea.png",
+        detail: "Japanese milk tea from Kirin.",
       },
       {
         title: "Royal Milk Tea",
         price: 2.99,
         image: "Drink/royalmilktea.png",
+        detail: "Japanese Royal milk tea"
       },
       {
         title: "Boss Cafe Au Lait",
         price: 2.99,
         image: "Drink/bosscafeaulait.png",
+        detail: "Japanese Boss Cafe Au Lait from Suntory."
       },
       {
         title: "Boss Black Coffee",
         price: 2.99,
         image: "Drink/bossblack.png",
+        detail: "Japanese Boss Black Coffee from Suntory."
       },
       {
         title: "Coke",
         price: 2.99,
         image: "Drink/Coke.png",
+        detail: "Coca-Cola.",
       },
       {
         title: "Pepsi",
         price: 2.99,
         image: "Drink/Pepsi.jpg",
+        detail: "Pepsi.",
       },
       {
         title: "Diet Coke",
         price: 2.99,
         image: "Drink/diet-coke.jpg",
+        detail: "Diet Coca-Cola.",
       },
       {
         title: "Diet Pepsi",
         price: 2.99,
         image: "Drink/diet-pepsi.jpg",
+        detail: "Diet Pepsi.",
       },
       {
         title: "Orange Crush",
         price: 2.99,
         image: "Drink/orange-crush.png",
+        detail: "Orange Crush"
       },
       {
         title: "Grape Crush",
         price: 2.99,
         image: "Drink/grape-crush.png",
+        detail: "Grape Crush"
       }
     ],
   ],
