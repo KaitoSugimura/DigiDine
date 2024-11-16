0392a229aa9f0d522873925e388622fbcd8349bb
Kaito Sugimura <sugimurakaito@gmail.com>
2024-11-15 19:01:17 -0700

    Fix end order screen

--- src/OrderConfirmedScreen.jsx
+++ src/OrderConfirmedScreen.jsx
@@ -2,24 +2,53 @@ import React from "react";
 import { useOrderContext, useScreenContext } from "./App";
 import styles from "./OrderConfirmedScreen.module.css";
 import OrderListComp from "./Main/orderListComp";
+import { formatPrice } from "./util";
 
 export default function OrderConfirmedScreen() {
   const { setScreen } = useScreenContext();
+  const { orderList } = useOrderContext();
 
   return (
     <div className={styles.root}>
-      <h1 className={styles.title}>Your order has been confirmed</h1>
+      <div className={styles.leftSide}>
+        <div className={styles.topCont}>
+          <h1 className={styles.title}>Thank you for your order!</h1>
+          <p>Your order will be delivered to your table shortly.</p>
+        </div>{" "}
+        <button
+          className={styles.doneButton}
+          onClick={() => {
+            setScreen("start");
+          }}
+        >
+          {"Done"}
+        </button>
+      </div>
+      <div className={styles.rightSide}>
+        <img src="./receipt.png" className={styles.receipt} />
 
-      <OrderListComp isFinal={true} />
-
-      <button
-        className={styles.doneButton}
-        onClick={() => {
-          setScreen("start");
-        }}
-      >
-        {"Done"}
-      </button>
+        <div className={styles.imageTop}>
+          <p className={styles.orderSum}>Order summary</p>
+          <div className={styles.confirmList}>
+            <OrderListComp isFinal={true} />
+          </div>
+          <div className={styles.priceCont}>
+            <p className={styles.totalText}>Total:</p>
+            <p className={styles.totalPrice}>
+              {formatPrice(
+                orderList.reduce(
+                  (acc, item) => acc + item.price * item.amount,
+                  0
+                )
+              )}
+            </p>
+            <span className={styles.taxInc}>{"(Tax included)"}</span>
+          </div>
+          <p className={styles.disclaim}>
+            You will receive a physical receipt with your order.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
